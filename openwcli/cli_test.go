d043cf3f2ffec24c7517dad7928af93d97ada0a9
麦志泉 <zhiquan911@qq.com>
2019-04-13 10:04:27 +0800

    clear test conf info

--- openwcli/cli_test.go
+++ openwcli/cli_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/astaxie/beego/config"
 	"github.com/blocktree/openwallet/log"
 	"github.com/blocktree/openwallet/owtp"
+	"path/filepath"
 )
 
 
@@ -13,55 +14,9 @@ func init() {
 
 func getTestOpenwCLI() *CLI {
 
-	confFile := `
-# Remote Server
-remoteserver = "120.78.83.180"
+	confFile := filepath.Join("conf", "node.ini")
 
-# API Version
-version = "1.0.0"
-
-# App Key
-appkey = "faa14b5e2cf119cd6d38bda45b49eb02b333a1b1ff6f10703acb554011ebfb1e"
-
-# App Secret
-appid = "8df7420d3917afa0172ea9c85e07ab55"
-
-# Log file path
-logdir = "./testdata/logs/"
-
-# Data directory, store keys, databases, backups
-datadir = "./testdata/data/"
-
-# Wallet Summary Period
-summaryperiod = "30s"
-
-# The custom name of local node
-localname = "blocktree"
-
-# Be trusted client server
-trustedserver = "127.0.0.1:9088"
-
-# Enable client server request local transfer
-enablerequesttransfer = true
-
-# Enable client server execute summary task
-enableexecutesummarytask = true
-
-# Enable client server edit wallet summary settings
-enableeditsummarysettings = true
-
-# Enable key agreement on local node communicate with client server
-enablekeyagreement = false
-
-# Enable https or wss
-enablessl = false
-
-# Network request timeout, unit: second
-requesttimeout = 120
-
-`
-
-	c, err := config.NewConfigData("ini", []byte(confFile))
+	c, err := config.NewConfig("ini", confFile)
 	if err != nil {
 		log.Error("NewConfigData error:", err)
 		return nil
